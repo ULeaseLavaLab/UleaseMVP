e2b697e30757ec688278b08b14e687f0b0065b30
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-04-19 19:56:11 -0700

    started reviews

---
+++ ulease/src/components/Review.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="d-flex align-center">
+      <v-avatar>
+        <img :src="pic">
+      </v-avatar>
+
+      <div>
+        <div class="t-button-text">{{ name }}</div>
+        <div class="t-small c-silver-text">{{ dateLeaseTerm }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Review',
+
+  props: {
+    name: { type: String, required: true },
+    pic: { type: String, required: true },
+    date: { type: Date, required: true },
+    leaseTerm: { type: Array, required: true },
+    text: { type: String, required: true },
+    helpfulCount: { type: Number, required: true },
+  },
+
+  computed: {
+    dateLeaseTerm() {
+      return `${this.date.toLocaleString()} Lease Term: `
+    },
+  },
+}
+</script>
--- ulease/src/components/Reviews.vue
+++ ulease/src/components/Reviews.vue
@@ -1,14 +1,43 @@
 <template>
-  
+  <div class="py-8">
+    <div class="t-h4 d-flex align-center mb-8"><v-icon class="c-blue-text">mdi-star</v-icon> <div>{{ `${rating} (${numReviews} reviews)` }}</div> </div>
+    <div class="d-flex align-center mb-4">
+      <v-text-field 
+        class="mr-2"
+        outlined 
+        placeholder="Search reviews" 
+        append-icon="mdi-magnify"
+        dense
+        hide-details
+      />
+      <v-btn
+        dark
+        color="var(--blue)"
+        class="t-button-text"
+      >Write a review</v-btn>
+    </div>
+
+    <Review
+      v-for="review, i in reviews"
+      :key="i"
+      v-bind="review"
+    />
+  </div>
 </template>
 
 <script>
+import Review from '@/components/Review.vue'
 export default {
   name: 'Reviews',
+
+  components: {
+    Review
+  },
 
   props: {
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
+    reviews: { type: Array, default: [] },
   },
 }
 </script>
